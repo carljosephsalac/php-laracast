fcfbdc1998a9df54417d9b77aac5013508466440
Carl Joseph Salac <salaccarljoseph@gmail.com>
2024-12-26 21:27:36 +0800

    Handle Multiple Request Methods From a Controller Action?

--- controllers/notes/create.php
+++ controllers/notes/create.php
@@ -19,6 +19,9 @@ if ($_SERVER["REQUEST_METHOD"] === 'POST') {
             'body' => $_POST['body'],
             'user_id' => 1
         ]);
+
+        header('location: /notes');
+        exit();
     }
 }
 
