0c10273a0352dfabfbb517b8b73f7c8f46cb3ef4
Carl Joseph Salac <salaccarljoseph@gmail.com>
2024-12-27 22:23:33 +0800

    Build a Better Router

--- views/notes/show.view.php
+++ views/notes/show.view.php
@@ -17,6 +17,7 @@
     <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
       <p><?= htmlspecialchars($note['body']) ?></p>
       <form class="mt-4" action="" method="POST">
+        <input type="hidden" name="_method" value="DELETE">
         <input type="hidden" name="id" value="<?= $note['id'] ?>">
         <button class="text-red-500" type="submit">Delete</button>
       </form>
