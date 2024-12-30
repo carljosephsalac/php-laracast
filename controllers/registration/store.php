69bfe6dfceaf19bd5aa7ef5e6c9eb93f478f9d9b
Carl Joseph Salac <salaccarljoseph@gmail.com>
2024-12-30 18:59:04 +0800

    Register a New User

---
+++ controllers/registration/store.php
@@ -0,0 +1,52 @@
+<?php
+
+use Core\Database;
+use Core\Validator;
+
+require base_path('Core/Validator.php');
+
+$config = require base_path('config.php');
+$db = new Database($config['database']);
+
+$email = $_POST['email'];
+$password = $_POST['password'];
+
+$errors = [];
+if (!Validator::string($email, 1, 30)) {
+    $errors['email'] = 'Email is required and no more than 20 characters';
+}
+if (!Validator::string($password, 1, 20)) {
+    $errors['password'] = 'Password is required and no more than 20 characters';
+}
+
+if (!empty($errors)) {
+    return view('registration/create.view.php', [
+        'heading' => 'Register',
+        'errors' => $errors
+    ]);
+}
+
+$user = $db->query('SELECT email FROM users WHERE email = :email', [
+    'email' => $email,
+])->find();
+
+if ($user) {
+    $_SESSION['user'] = [
+        'email' => $email,
+    ];
+    
+    header('location: /');
+    exit();
+} else {
+    $db->query('INSERT INTO users (email, password) VALUES (:email, :password)', [
+        'email' => $email,
+        'password' => $password,
+    ]);
+
+    $_SESSION['user'] = [
+        'email' => $email,
+    ];
+
+    header('location: /');
+    exit();
+}
