eb45ae38e472049006114b5cd6c3333437bc500c
Carl Joseph Salac <salaccarljoseph@gmail.com>
2024-12-31 10:45:45 +0800

    Manage Passwords Like This For The Remainder of Your Career

--- views/index.view.php
+++ views/index.view.php
@@ -15,7 +15,7 @@
 
   <main>
     <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-      <p>Home</p>
+      <p>Hello <?= $_SESSION['user']['email'] ?? 'Guest' ?>. Welcome to the home page.</p>
     </div>
   </main>
 </div>
